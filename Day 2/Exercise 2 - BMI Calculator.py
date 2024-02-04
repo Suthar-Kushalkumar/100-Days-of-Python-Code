40c8eb0fa9abc9d95dc303345ebc743d1511ac96
Suthar Kushalkumar <111581593+Suthar-Kushalkumar@users.noreply.github.com>
2024-02-04 13:48:04 +0530

    Add files via upload

---
+++ Day 2/Exercise 2 - BMI Calculator.py
@@ -0,0 +1,7 @@
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+print(int(float(weight)/(float(height)*float(height))))
+
+from time import sleep
+sleep(10)
