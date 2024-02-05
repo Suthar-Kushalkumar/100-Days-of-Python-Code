bcfc6c8ceac631d17b90b04db15d098d2e60d98f
Suthar Kushalkumar <111581593+Suthar-Kushalkumar@users.noreply.github.com>
2024-02-05 10:18:42 +0530

    Add files via upload

---
+++ Day 3/Day 3 Project- Treasure Island.py
@@ -0,0 +1,43 @@
+#Flow chart of the game: https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+if input("You're at a cross road. Where do you want to go? Type left or right:\n ")=="right":
+    print("\nYou fell into a hole. Game over.")
+else:
+    if input("\nYou came to a lake. There is an island in the middle of the lake. Type wait to wait for a boat. Type swim to swim across.\n ")=="swim":
+        print("\nYou drowned. Game over.")
+    else:
+        door=input("\nYou arrive at the island unharmed. There is a house with doors. One red, one yellow and one blue. Which colour do you choose?\n")
+        if door=="blue":
+            print("\nYou enter a room of beasts. Game over.")
+        elif door=="red":
+            print("\nYou enter a room of fire. Game over.")
+        else:
+            print("\nYou found tresure. You won.\n")
+            print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+from time import sleep
+sleep(10)
